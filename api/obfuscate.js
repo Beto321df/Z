fba098b86b9aae8e696ceb59d0928dedf5c16daf
Beto321df <h4ck666q@gmail.com>
2026-09-03 02:23:10 -0500

    Update response to include obfuscatedCode
    Added 'obfuscatedCode' to the response object to match HTML expectations.

--- api/obfuscate.js
+++ api/obfuscate.js
@@ -1,13 +1,11 @@
+
 const CodeGenerator = require('../src/generator/codegen.js');
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
     if (req.method === 'OPTIONS') {
         res.status(200).end();
@@ -23,7 +21,8 @@ module.exports = async (req, res) => {
 
         res.status(200).json({
             success: true,
-            code: obfuscatedCode
+            code: obfuscatedCode,
+            obfuscatedCode: obfuscatedCode // <--- Añadimos esto para que coincida con el HTML
         });
     } catch (error) {
         res.status(500).json({
