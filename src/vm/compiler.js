217bdd63b9060cbb96dae0cc351c057319c2ce28
Beto321df <h4ck666q@gmail.com>
2026-09-03 02:10:27 -0500

    Refactor VMCompiler and add BytecodeCompiler

--- src/vm/compiler.js
+++ src/vm/compiler.js
@@ -1,36 +1,47 @@
-class VMCompiler {
+// Opcodes Dinámicos
+const OPCODES = {
+    LOADCONST: 0x01,
+    GETGLOBAL: 0x02,
+    CALL:      0x03,
+    SETLOCAL:  0x04,
+    RETURN:    0x05
+};
+
+class BytecodeCompiler {
     compile(ast) {
-        const bytecode = [];
+        const constants = [];
+        const instructions = [];
 
-        if (!ast) return bytecode;
+        const addConst = (val) => {
+            let idx = constants.indexOf(val);
+            if (idx === -1) {
+                constants.push(val);
+                idx = constants.length - 1;
+            }
+            return idx;
+        };
 
-        const statements = ast.body || (Array.isArray(ast) ? ast : [ast]);
+        for (const node of ast.body) {
+            if (node.type === 'CallStatement') {
+                // Cargar función global (ej. print)
+                const fnIdx = addConst(node.name);
+                instructions.push([OPCODES.GETGLOBAL, 0, fnIdx]);
 
-        for (const node of statements) {
-            if (!node) continue;
+                // Cargar argumentos
+                node.args.forEach((arg, i) => {
+                    const argIdx = addConst(arg.value);
+                    instructions.push([OPCODES.LOADCONST, i + 1, argIdx]);
+                });
 
-            // Procesar llamadas a funciones como print(...)
-            if (node.type === 'CallExpression' || (node.expression && node.expression.type === 'CallExpression')) {
-                const callNode = node.type === 'CallExpression' ? node : node.expression;
-                const fnName = callNode.callee ? callNode.callee.name : 'print';
-                
-                let argValue = null;
-                if (callNode.arguments && callNode.arguments.length > 0) {
-                    const firstArg = callNode.arguments[0];
-                    argValue = firstArg.value || firstArg.raw || firstArg.name || null;
-                }
-
-                // Generar bytecode: [2, nombreFuncion, argumento]
-                bytecode.push([2, fnName, argValue]);
+                // Llamar función: [OPCODE, reg_fn, num_args]
+                instructions.push([OPCODES.CALL, 0, node.args.length]);
             }
         }
 
-        if (bytecode.length === 0) {
-            bytecode.push([2, "print", "Hola desde Roblox"]);
-        }
+        instructions.push([OPCODES.RETURN, 0, 0]);
 
-        return bytecode;
+        return { constants, instructions };
     }
 }
 
-module.exports = VMCompiler;
+module.exports = { BytecodeCompiler, OPCODES };
