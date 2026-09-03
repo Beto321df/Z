3f408a933d6e62c73935e2aae91a70031252b0a1
Beto321df <h4ck666q@gmail.com>
2026-09-03 02:09:22 -0500

    Simplify ASTNode construction with properties
    Refactored ASTNode class to accept properties as an object.

--- src/parser/ast.js
+++ src/parser/ast.js
@@ -1,75 +1,8 @@
 class ASTNode {
-    constructor(type) {
+    constructor(type, props = {}) {
         this.type = type;
+        Object.assign(this, props);
     }
 }
 
-class ChunkNode extends ASTNode {
-    constructor(body = []) {
-        super('Chunk');
-        this.body = body;
-    }
-}
-
-class LocalStatementNode extends ASTNode {
-    constructor(names = [], init = []) {
-        super('LocalStatement');
-        this.names = names;
-        this.init = init;
-    }
-}
-
-class AssignmentStatementNode extends ASTNode {
-    constructor(variables = [], init = []) {
-        super('AssignmentStatement');
-        this.variables = variables;
-        this.init = init;
-    }
-}
-
-class CallStatementNode extends ASTNode {
-    constructor(expression) {
-        super('CallStatement');
-        this.expression = expression;
-    }
-}
-
-class FunctionCallNode extends ASTNode {
-    constructor(callee, args = []) {
-        super('FunctionCall');
-        this.callee = callee;
-        this.args = args;
-    }
-}
-
-class IdentifierNode extends ASTNode {
-    constructor(name) {
-        super('Identifier');
-        this.name = name;
-    }
-}
-
-class StringLiteralNode extends ASTNode {
-    constructor(value) {
-        super('StringLiteral');
-        this.value = value;
-    }
-}
-
-class NumberLiteralNode extends ASTNode {
-    constructor(value) {
-        super('NumberLiteral');
-        this.value = value;
-    }
-}
-
-module.exports = {
-    ChunkNode,
-    LocalStatementNode,
-    AssignmentStatementNode,
-    CallStatementNode,
-    FunctionCallNode,
-    IdentifierNode,
-    StringLiteralNode,
-    NumberLiteralNode
-};
+module.exports = ASTNode;
