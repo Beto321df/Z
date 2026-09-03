e5c6003e273dc005f57ecb33145c45f2c6fb969b
Beto321df <h4ck666q@gmail.com>
2026-09-02 21:37:04 -0500

    Update tokenizer.js

--- src/lexer/tokenizer.js
+++ src/lexer/tokenizer.js
@@ -1,115 +1,123 @@
-import { TokenType, Token } from './tokens.js';
+const { TokenType, LUAU_KEYWORDS } = require('./tokens');
 
-export class Tokenizer {
-  constructor(input) {
-    this.input = input;
-    this.position = 0;
-    this.line = 1;
-    this.column = 1;
-  }
-
-  peek() {
-    return this.position < this.input.length ? this.input[this.position] : null;
-  }
-
-  advance() {
-    const char = this.peek();
-    this.position++;
-    if (char === '\n') {
-      this.line++;
-      this.column = 1;
-    } else {
-      this.column++;
-    }
-    return char;
-  }
-
-  tokenize() {
-    const tokens = [];
-
-    while (this.position < this.input.length) {
-      const char = this.peek();
-
-      // Ignorar espacios en blanco y saltos de línea
-      if (/\s/.test(char)) {
-        this.advance();
-        continue;
-      }
-
-      const startLine = this.line;
-      const startCol = this.column;
-
-      // Números (enteros y decimales)
-      if (/[0-9]/.test(char)) {
-        let value = '';
-        while (this.peek() && /[0-9.]/.test(this.peek())) {
-          value += this.advance();
-        }
-        tokens.push(new Token(TokenType.NUMBER, value, startLine, startCol));
-        continue;
-      }
-
-      // Identificadores y palabras clave
-      if (/[a-zA-Z_]/.test(char)) {
-        let value = '';
-        while (this.peek() && /[a-zA-Z0-9_]/.test(this.peek())) {
-          value += this.advance();
-        }
-        tokens.push(new Token(TokenType.IDENTIFIER, value, startLine, startCol));
-        continue;
-      }
-
-      // Cadenas de texto ("ejemplo")
-      if (char === '"' || char === "'") {
-        const quote = this.advance();
-        let value = '';
-        while (this.peek() && this.peek() !== quote) {
-          value += this.advance();
-        }
-        this.advance(); // Consumir comilla de cierre
-        tokens.push(new Token(TokenType.STRING, value, startLine, startCol));
-        continue;
-      }
-
-      // Operadores y símbolos individuales
-      switch (char) {
-        case '=':
-          tokens.push(new Token(TokenType.ASSIGN, this.advance(), startLine, startCol));
-          break;
-        case '+':
-          tokens.push(new Token(TokenType.PLUS, this.advance(), startLine, startCol));
-          break;
-        case '-':
-          tokens.push(new Token(TokenType.MINUS, this.advance(), startLine, startCol));
-          break;
-        case '*':
-          tokens.push(new Token(TokenType.STAR, this.advance(), startLine, startCol));
-          break;
-        case '/':
-          tokens.push(new Token(TokenType.SLASH, this.advance(), startLine, startCol));
-          break;
-        case '(':
-          tokens.push(new Token(TokenType.LPAREN, this.advance(), startLine, startCol));
-          break;
-        case ')':
-          tokens.push(new Token(TokenType.RPAREN, this.advance(), startLine, startCol));
-          break;
-        case '{':
-          tokens.push(new Token(TokenType.LBRACE, this.advance(), startLine, startCol));
-          break;
-        case '}':
-          tokens.push(new Token(TokenType.RBRACE, this.advance(), startLine, startCol));
-          break;
-        case ';':
-          tokens.push(new Token(TokenType.SEMICOLON, this.advance(), startLine, startCol));
-          break;
-        default:
-          tokens.push(new Token(TokenType.UNKNOWN, this.advance(), startLine, startCol));
-          break;
-      }
+class Tokenizer {
+    constructor(input) {
+        this.input = input;
+        this.pos = 0;
+        this.length = input.length;
     }
 
-    tokens.push(new Token(TokenType.EOF, '', this.line, this.column));
-    return tokens;
-  }
+    tokenize() {
+        const tokens = [];
+
+        while (this.pos < this.length) {
+            const char = this.input[this.pos];
+
+            if (/\s/.test(char)) {
+                this.pos++;
+                continue;
+            }
+
+            if (char === '-' && this.input[this.pos + 1] === '-') {
+                this.skipComment();
+                continue;
+            }
+
+            if (char === '"' || char === "'" || (char === '[' && this.input[this.pos + 1] === '[')) {
+                tokens.push(this.readString(char));
+                continue;
+            }
+
+            if (/\d/.test(char)) {
+                tokens.push(this.readNumber());
+                continue;
+            }
+
+            if (/[a-zA-Z_]/.test(char)) {
+                tokens.push(this.readIdentifier());
+                continue;
+            }
+
+            tokens.push(this.readSymbol());
+        }
+
+        tokens.push({ type: TokenType.EOF, value: null });
+        return tokens;
+    }
+
+    skipComment() {
+        this.pos += 2;
+        if (this.input.substring(this.pos, this.pos + 2) === '[[') {
+            this.pos += 2;
+            while (this.pos < this.length && this.input.substring(this.pos, this.pos + 2) !== ']]') {
+                this.pos++;
+            }
+            this.pos += 2;
+        } else {
+            while (this.pos < this.length && this.input[this.pos] !== '\n') {
+                this.pos++;
+            }
+        }
+    }
+
+    readString(quote) {
+        if (quote === '[') {
+            this.pos += 2;
+            let valStart = this.pos;
+            while (this.pos < this.length && this.input.substring(this.pos, this.pos + 2) !== ']]') {
+                this.pos++;
+            }
+            let str = this.input.substring(valStart, this.pos);
+            this.pos += 2;
+            return { type: TokenType.STRING, value: str };
+        } else {
+            this.pos++;
+            let str = '';
+            while (this.pos < this.length && this.input[this.pos] !== quote) {
+                if (this.input[this.pos] === '\\') {
+                    str += this.input[this.pos] + (this.input[this.pos + 1] || '');
+                    this.pos += 2;
+                } else {
+                    str += this.input[this.pos];
+                    this.pos++;
+                }
+            }
+            this.pos++;
+            return { type: TokenType.STRING, value: str };
+        }
+    }
+
+    readNumber() {
+        let start = this.pos;
+        while (this.pos < this.length && /[0-9.xXA-Fa-f]/.test(this.input[this.pos])) {
+            this.pos++;
+        }
+        return { type: TokenType.NUMBER, value: this.input.substring(start, this.pos) };
+    }
+
+    readIdentifier() {
+        let start = this.pos;
+        while (this.pos < this.length && /[a-zA-Z0-9_]/.test(this.input[this.pos])) {
+            this.pos++;
+        }
+        const value = this.input.substring(start, this.pos);
+        const type = LUAU_KEYWORDS.has(value) ? TokenType.KEYWORD : TokenType.IDENTIFIER;
+        return { type, value };
+    }
+
+    readSymbol() {
+        const twoChar = this.input.substring(this.pos, this.pos + 2);
+        const doubleOps = ['==', '~=', '<=', '>=', '..', '+=', '-=', '*=', '/='];
+
+        if (doubleOps.includes(twoChar)) {
+            this.pos += 2;
+            return { type: TokenType.SYMBOL, value: twoChar };
+        }
+
+        const char = this.input[this.pos];
+        this.pos++;
+        return { type: TokenType.SYMBOL, value: char };
+    }
 }
+
+module.exports = Tokenizer;
