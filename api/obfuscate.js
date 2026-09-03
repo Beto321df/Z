0baef379effc84b4bbc684fdf5348ce21a213c58
Beto321df <h4ck666q@gmail.com>
2026-09-02 21:18:23 -0500

    Update obfuscate.js

--- api/obfuscate.js
+++ api/obfuscate.js
@@ -1,1 +1,56 @@
+const Tokenizer = require('../src/lexer/tokenizer');
+const Parser = require('../src/parser/parser');
+const StringEncoderTransform = require('../src/transforms/stringEncoder');
+const DeadCodeTransform = require('../src/transforms/deadCode');
+const VMCompiler = require('../src/vm/compiler');
+const VMInterpreterGenerator = require('../src/vm/interpreter');
+const CodeGenerator = require('../src/generator/codegen');
 
+module.exports = async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Método no permitido' });
+    }
+
+    try {
+        const { code } = req.body;
+        if (!code) {
+            return res.status(400).json({ error: 'Código no proporcionado' });
+        }
+
+        // Pipeline del Ofuscador
+        const tokenizer = new Tokenizer(code);
+        const tokens = tokenizer.tokenize();
+
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+
+        const stringTable = new Map();
+        const stringEncoder = new StringEncoderTransform();
+        stringEncoder.transformNode(ast, stringTable);
+
+        const deadCode = new DeadCodeTransform();
+        if (ast.body) {
+            ast.body = deadCode.inject(ast.body);
+        }
+
+        const compiler = new VMCompiler();
+        const bytecode = compiler.compile(ast);
+
+        const interpreterGen = new VMInterpreterGenerator();
+        const vmCode = interpreterGen.generate(bytecode, stringTable);
+
+        const generator = new CodeGenerator();
+        const obfuscatedCode = generator.generate(vmCode);
+
+        return res.status(200).json({
+            success: true,
+            obfuscatedCode: obfuscatedCode
+        });
+    } catch (err) {
+        console.error('Error durante la ofuscación:', err);
+        return res.status(500).json({
+            error: 'Error interno en el motor de ofuscación',
+            details: err.message
+        });
+    }
+};
