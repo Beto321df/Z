5e0e92c74ddd356180a0283c59b26f8b534eccde
Beto321df <h4ck666q@gmail.com>
2026-09-02 21:30:20 -0500

    Update obfuscate.js

--- api/obfuscate.js
+++ api/obfuscate.js
@@ -7,6 +7,20 @@ const VMInterpreterGenerator = require('../src/vm/interpreter');
 const CodeGenerator = require('../src/generator/codegen');
 
 module.exports = async (req, res) => {
+    // Encabezados para permitir CORS en cualquier origen y método
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Responder llamadas preflight (OPTIONS)
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método no permitido' });
     }
