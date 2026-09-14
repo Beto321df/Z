c66d0d019644714b06153983477d2d3867b11188
Beto321df <h4ck666q@gmail.com>
2026-09-14 18:59:27 -0500

    Route legacy VM compiler through Z-Lang 3

--- src/vm/compiler.js
+++ src/vm/compiler.js
@@ -1,26 +1,24 @@
-const { tokenize } = require('../zlang/lexer');
-const { ZCompiler } = require('../zlang/compiler');
+const { buildProgram, OPS, BIN, UNARY } = require('../zlang/compiler3');
+const { encodeProgram } = require('../zlang/format3');
 
 class BytecodeCompiler {
-    constructor() {
-        this.constants = [];
-        this.instructions = [];
-        this.registerCount = 1;
-    }
-
     compileSource(source) {
-        const tokens = tokenize(source);
-        const bytecode = new ZCompiler().compile(tokens);
-        this.instructions = Array.from(bytecode, value => ['DATA', value]);
-        this.constants = [];
-        return { constants: this.constants, instructions: this.instructions, ir: bytecode };
+        const program = buildProgram(source);
+        const bytecode = encodeProgram(program);
+        return {
+            constants: program.constants,
+            functions: program.functions,
+            program,
+            bytecode,
+            instructions: program.functions[program.root].code
+        };
     }
 
     compile(astOrSource) {
         if (typeof astOrSource === 'string') return this.compileSource(astOrSource);
         if (astOrSource && typeof astOrSource.source === 'string') return this.compileSource(astOrSource.source);
-        throw new Error('El compilador legado ahora requiere código fuente para entrar al pipeline Z-Lang.');
+        throw new Error('Z-Lang 3 compiler requiere código fuente Lua/Luau.');
     }
 }
 
-module.exports = { BytecodeCompiler };
+module.exports = { BytecodeCompiler, OPS, BIN, UNARY };
