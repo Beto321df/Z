905ad767f0aeadffad42f26635b1507f6bfc8dc1
Beto321df <h4ck666q@gmail.com>
2026-09-15 12:15:16 -0600

    expose explicit Z-IR pipeline helpers

--- src/zlang/index.js
+++ src/zlang/index.js
@@ -3,29 +3,23 @@ const LuauParser = require('../parser/luauParser');
 const Tokenizer = require('../lexer/tokenizer');
 const { encodeProgram } = require('./format3');
 const { encodeBytecode, decodeBytecode, ALPHABET, checksum } = require('./codec');
+const { programToIR, validateIR, irToProgram, OP_NAMES } = require('./ir3');
 
-function parse(source) {
-    return new LuauParser(source).parse();
-}
-
-function lex(source) {
-    return new Tokenizer(source).tokenize();
-}
-
+function parse(source) { return new LuauParser(source).parse(); }
+function lex(source) { return new Tokenizer(source).tokenize(); }
 function compile(source) {
     const program = buildProgram(source);
-    return { program, bytecode: encodeProgram(program) };
+    const ir = programToIR(program);
+    validateIR(ir);
+    return { program: irToProgram(ir), ir, bytecode: encodeProgram(program) };
 }
 
 module.exports = {
     OPS, BIN, UNARY, ALPHABET, checksum,
-    Tokenizer,
-    LuauParser,
-    lex,
-    parse,
-    buildProgram,
-    encodeProgram,
-    encodeBytecode,
-    decodeBytecode,
+    OP_NAMES,
+    Tokenizer, LuauParser,
+    lex, parse, buildProgram,
+    programToIR, validateIR, irToProgram,
+    encodeProgram, encodeBytecode, decodeBytecode,
     compile
 };
