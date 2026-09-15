220c5d44e4c2cec09c0adde8d53dbb39b7217cf4
Beto321df <h4ck666q@gmail.com>
2026-09-15 12:08:24 -0600

    make Z-native compiler the public pipeline

--- src/zlang/index.js
+++ src/zlang/index.js
@@ -1,21 +1,28 @@
-const { OPS, BIN, UNARY, buildProgram } = require('./compiler3');
+const { OPS, BIN, UNARY, buildProgram } = require('./nativeCompiler3');
+const LuauParser = require('../parser/luauParser');
+const Tokenizer = require('../lexer/tokenizer');
 const { encodeProgram } = require('./format3');
 const { encodeBytecode, decodeBytecode, ALPHABET, checksum } = require('./codec');
 
+function parse(source) {
+    return new LuauParser(source).parse();
+}
+
+function lex(source) {
+    return new Tokenizer(source).tokenize();
+}
+
 function compile(source) {
     const program = buildProgram(source);
-    return {
-        program,
-        bytecode: encodeProgram(program)
-    };
+    return { program, bytecode: encodeProgram(program) };
 }
 
 module.exports = {
-    OPS,
-    BIN,
-    UNARY,
-    ALPHABET,
-    checksum,
+    OPS, BIN, UNARY, ALPHABET, checksum,
+    Tokenizer,
+    LuauParser,
+    lex,
+    parse,
     buildProgram,
     encodeProgram,
     encodeBytecode,
